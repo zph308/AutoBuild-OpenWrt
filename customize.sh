f2126929065ae54f8fea803d7ebe78d0c1e315ff
zph308 <63138532+zph308@users.noreply.github.com>
2021-03-13 22:58:35 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -5,5 +5,8 @@
 # Author: eSirPlayground
 # Youtube Channel: https://goo.gl/fvkdwm 
 #=================================================
-#1. Modify default IP
-sed -i 's/192.168.1.1/192.168.5.1/g' openwrt/package/base-files/files/bin/config_generate
+#1. 修改默认的IP
+sed -i 's/192.168.1.1/192.168.1.1/g' openwrt/package/base-files/files/bin/config_generate
+
+#2. 设置密码为空
+sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' openwrt/package/lean/default-settings/files/zzz-default-settings
